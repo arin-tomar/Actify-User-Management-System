c537867646dd66a25bdd2faff73b5697ae295661
arin-tomar <arintomar2004@gmail.com>
2026-09-12 10:13:14 +0530

    Create ActifyApplication.java

---
+++ src/main/resources/src/main/java/com/actify/ums/ActifyApplication.java
@@ -0,0 +1,12 @@
+package com.actify.ums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActifyApplication.class, args);
+    }
+}
